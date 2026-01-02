fa5e39ac25182d4716a3b1531da5e2271d715031
Perezented <MPerez6473@Gmail.com>
2026-01-02 16:18:22 -0600

    - Refactor patient fetching function to use async/await and enhance IPatient interface - fetchPatientsFromAPI now can fetch pages and limits through params - Addition of comments

--- index.ts
+++ index.ts
@@ -6,23 +6,42 @@ interface IResults {
   data_quality_issues: string[]
 };
 
+interface IPatient {
+  patient_id: string,
+  name?: string,
+  age?: number | string,
+  gender?: string,
+  blood_pressure?: string,
+  temperature?: number | string,
+  visit_date?: string,
+  diagnosis?: string,
+  medications?: string
+}
+
 const results: IResults = {
   high_risk_patients: [],
   fever_patients: [],
   data_quality_issues: []
 };
 
-function fetchPatientsFromAPI() {
-  fetch("https://assessment.ksensetech.com/api/patients?page=1&limit=10", {
+// Fetch patients from the API with pagination
+// Default to page 1 and limit 10
+// Returns a promise that resolves to the list of patients
+async function fetchPatientsFromAPI(page: number = 1, limit: number = 10): Promise<IPatient[]> {
+  const response = await fetch(`https://assessment.ksensetech.com/api/patients?page=${page}&limit=${limit}`, {
     method: "GET",
     headers: {
       "x-api-key": process.env.API_KEY || ""
     }
-  })
-    .then(response => response.json())
-    .then(data => console.log(data));
+  });
+  const data = await response.json();
+  console.log(data);
+  return data.patients;
 }
 
+// Process patients to identify high risk, fever, and data quality issues
+
+// submit the results to the API (3 total attempts)
 const submitResult = () => {
   fetch("https://assessment.ksensetech.com/api/submit-assessment", {
     method: "POST",
@@ -36,4 +55,4 @@ const submitResult = () => {
     .then(data => console.log("Assessment Results:", data));
 }
 
-fetchPatientsFromAPI();
+// fetchPatientsFromAPI();
